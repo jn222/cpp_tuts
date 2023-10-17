fc84d3f520f53f33da03c64c4728ce7727fdd7cf
jn222 <jonathanng222@gmail.com>
2023-10-17 14:04:22 -0400

    hello world, input output, version compatibility test

---
+++ inputoutput/inputoutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+    cout << "please enter your name.\n";
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << "!\n";
+    return 0;
+}
